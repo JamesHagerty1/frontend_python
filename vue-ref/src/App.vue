852ac4ea9ad5fd0472ed1fc979e8fbfd06bcadb2
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-11-14 12:21:15 -0500

    spacing

--- vue-ref/src/App.vue
+++ vue-ref/src/App.vue
@@ -3,37 +3,37 @@ A minimal Vue app using Vue Single File Components.
 -->
 
 <template>
-  <div id="hello">
-    Hello!
-  </div>
-  <div>
-    Count: {{ count }}
-  </div>
-  <div id="clickme" @click="increment">
-    Click me to raise count!
-  </div>
+    <div id="hello">
+        Hello!
+    </div>
+    <div>
+        Count: {{ count }}
+    </div>
+    <div id="clickme" @click="increment">
+        Click me to raise count!
+    </div>
 </template>
 
 <style scoped>
-  #hello {
-    background-color: green;
-    width: 120px;
-    font-size: 24px;
-  }
+    #hello {
+        background-color: green;
+        width: 120px;
+        font-size: 24px;
+    }
 
-  #clickme {
-    background-color: gray;
-    width: 100px;
-    font-size: 20px;
-  }
+    #clickme {
+        background-color: gray;
+        width: 100px;
+        font-size: 20px;
+    }
 </style>
 
 <script setup>
-  import { ref } from 'vue'
+    import { ref } from 'vue'
 
-  const count = ref(0)
+    const count = ref(0)
 
-  function increment() {
-    count.value++
-  }
+    function increment() {
+        count.value++
+    }
 </script>
