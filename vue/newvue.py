14a2e1da5b9ca6617f112e4d9b39327396f0319b
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-11-21 18:19:08 -0500

    newvue script and deletion of stuff in vue base to make it work

---
+++ vue/newvue.py
@@ -0,0 +1,39 @@
+"""
+% python newvue.py app_name app_folder_path
+"""
+
+
+import argparse
+import os
+import shutil
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Initialize a Vue app")
+
+    parser.add_argument("name", type=str, help="Name of the new Vue app folder")
+
+    parser.add_argument("destination", type=str, 
+                        help="Path to the destination folder")
+    
+    args = parser.parse_args()
+
+    try:
+        if not os.path.isdir(args.destination):
+            print(f"Destination folder '{args.destination}' does not exist.")
+            return
+        
+        app_path = os.path.join(args.destination, args.name)
+
+        shutil.copytree("./copy_base_vue_app", app_path)
+
+        os.chdir(app_path)
+
+        #os.system("npm install && npm run dev")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
