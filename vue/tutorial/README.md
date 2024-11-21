d7c5606ecb0e6264ec8aa6a99c10215fc5e94102
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-11-21 18:53:26 -0500

    declarative rendering

---
+++ vue/tutorial/README.md
@@ -0,0 +1,4 @@
+The examples ripped straight from the official Vue tutorial:
+https://vuejs.org/tutorial/#step-1
+
+For the most part, just look at src/App.vue for each of the folders in here.
--- vue/tutorial/declarative_rendering/src/App.vue
+++ vue/tutorial/declarative_rendering/src/App.vue
@@ -2,11 +2,22 @@
 // TODO figure out how to user vue-router
     // import { RouterLink, RouterView } from 'vue-router'
 
+    import { reactive, ref } from 'vue'
+
+    const counter = reactive({ count: 42 })
+    const message = ref('Hello World!')
 </script>
 
+
 <template>
-    <div>Hello Vue</div>
+    <!--
+    Notice, no need to include .value for objects with reactivity, in the
+    template.
+    -->
+    <h1>{{ message }}</h1>
+    <p>Count is: {{ counter.count }}</p>
 </template>
+
 
 <style scoped>
 
